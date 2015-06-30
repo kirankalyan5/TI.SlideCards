91c394ab687ce681a22bb9b943fd6800061265ed
K, KiranKumar (US - Bengaluru) <kirk@DELOITTE.com>
2015-06-30 20:13:53 +0530

    Added dynamic horizontal card blocks

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,81 @@
 //Instantiate slideCards
 var slideCards = Alloy.createWidget("SlideCards", {
 	cards : [ {
-		parentView: "",
-		childView: ""
-	} ],
+		parentView : Ti.UI.createView({
+			left : 0,
+			height : "30dp",
+			width : "30dp",
+			backgroundColor : "red"
+		}),
+		childView : Ti.UI.createView({
+			right : 0,
+			height : "30dp",
+			width : "30dp",
+			backgroundColor : "yellow"
+		})
+	}, {
+		parentView : Ti.UI.createView({
+			left : 0,
+			height : "30dp",
+			width : "30dp",
+			backgroundColor : "red"
+		}),
+		childView : Ti.UI.createView({
+			right : 0,
+			height : "30dp",
+			width : "30dp",
+			backgroundColor : "yellow"
+		})
+	}, {
+		parentView : Ti.UI.createView({
+			left : 0,
+			height : "30dp",
+			width : "30dp",
+			backgroundColor : "pink"
+		}),
+		childView : Ti.UI.createView({
+			right : 0,
+			height : "30dp",
+			width : "30dp",
+			backgroundColor : "yellow"
+		})
+	},
+	{
+		parentView : Ti.UI.createView({
+			left : 0,
+			height : "30dp",
+			width : "30dp",
+			backgroundColor : "pink"
+		}),
+		childView : Ti.UI.createView({
+			right : 0,
+			height : "30dp",
+			width : "30dp",
+			backgroundColor : "yellow"
+		})
+	},
+	{
+		parentView : Ti.UI.createView({
+			left : 0,
+			height : "30dp",
+			width : "30dp",
+			backgroundColor : "pink"
+		}),
+		childView : Ti.UI.createView({
+			right : 0,
+			height : "30dp",
+			width : "30dp",
+			backgroundColor : "yellow"
+		})
+	}],
 	style : {
-		height: "400dp",
-		width: "300dp",
-		spacing: ["10dp", "10dp"] //top/bottom, left/right
+		height : "400dp",
+		width : "300dp",
+		child : {
+			//height : "200dp",
+			width : "100dp"
+		},
+		spacing : [ "10dp", "10dp" ] // top/bottom, left/right
 	}
 });
 
--- app/widgets/SlideCards/controllers/widget.js
+++ app/widgets/SlideCards/controllers/widget.js
@@ -26,14 +26,40 @@ var style = args.style;
 	};
 	
 	//Create _buildCards
-	var _buildCards = function (cards) {
-		console.log(cards);
+	var _buildCards = function (cards, style) {
+		var cardStyle = {
+			height: style.child ? style.child.height : undefined,
+			width: style.child ? style.child.width : undefined		
+		};
+		var spacing = style.spacing || ["10dp", "10dp"];
+		var offsetHeight = parseInt(spacing[0].replace(/\D/g,''));
+		var offsetWidth = parseInt(spacing[1].replace(/\D/g,''));
+		var height = cardStyle.height ? cardStyle.height : parseInt(style.height.replace(/\D/g,'')) - (offsetHeight * 2);
+		var width = cardStyle.width ? cardStyle.width : parseInt(style.width.replace(/\D/g,'')) - (offsetWidth * 2);
+		
+		//Iterate and build the card  block
+		_.each(cards, function (card, index) {
+			var panel = Ti.UI.createView({
+				height: height + "dp" || Ti.UI.SIZE,
+				width: width + "dp" || Ti.UI.SIZE,
+				top: "10dp",
+				left: "10dp",
+				right: "10dp",
+				bottom: "10dp",
+				backgroundColor: "blue"
+			});
+			panel.add(card.parentView);
+			panel.add(card.childView);
+			scrollView.add(panel);
+		});
+			
+
 	};
 	
 	//Initialize Method
 	var init = function () {
 		_applyConfigStyles(style);
-		_buildCards(cards);
+		_buildCards(cards, style);
 	};
 	
 	//initialize the widget
